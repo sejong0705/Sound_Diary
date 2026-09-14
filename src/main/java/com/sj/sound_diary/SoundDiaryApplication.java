b26f112925e1eba00139962cbb65f0446a27175c
sejong0705 <lsj010705@gmail.com>
2026-09-14 20:40:31 +0900

    feat: 개발 환경 구축 및 DB 연동 확인

---
+++ src/main/java/com/sj/sound_diary/SoundDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.sj.sound_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundDiaryApplication.class, args);
+    }
+
+}
